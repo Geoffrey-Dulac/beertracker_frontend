a66d99f3c775d60fa022cf86d7cda5e30c9f1ce1
macbook <geoffrey.dulac2@gmail.com>
2021-08-10 13:06:56 +0200

    deleted all that is useless + colors palette integrated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import './App.css';
+import './scss/app.scss';
+import React from 'react';
+import Loginpage from './pages/Loginpage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Loginpage/>
     </div>
   );
 }
