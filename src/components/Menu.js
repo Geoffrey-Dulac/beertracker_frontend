e60c8c78d791a67754dcf46e121aaa376367a41a
macbook <geoffrey.dulac2@gmail.com>
2021-08-14 18:13:40 +0200

    component menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-function Menu() {
-    function handleClick() {
-        console.log('click');
+function Menu(e) {
+    function handleClick(e) {
+        document.querySelector('.hamburger hr:nth-child(1)').classList.toggle('rotate-first-HR');
+        document.querySelector('.hamburger hr:nth-child(2)').classList.toggle('rotate-second-HR');
+        document.querySelector('.hamburger hr:nth-child(3)').classList.toggle('rotate-third-HR');
+        document.querySelector('.background-menu').classList.toggle('display-background-menu');
+        document.querySelector('.menu-items').classList.toggle('display-menu-items');
     }
 
     return (
-        <div className='hamburger' onClick={handleClick}>
-            <hr/>
-            <hr/>
-            <hr/>
+        <div>        
+            <div className='hamburger' onClick={(e) => handleClick(e)}>
+                <hr/>
+                <hr/>
+                <hr/>
+            </div>
+            <div className='background-menu'></div>
+            <div className='menu-items'>
+                <Link to="/"><p className='text-dark mt-2'>Page 1</p></Link>
+                <Link to="/"><p className='text-dark mt-2'>Page 2</p></Link>
+                <Link to="/"><p className='text-dark mt-2'>Page 3</p></Link>
+                <Link to="/"><p className='text-dark mt-2'>Page 4</p></Link>
+            </div>
         </div>
     );
 }
